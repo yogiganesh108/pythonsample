e12f8a3bbb4cebf6585d87c6f7f42f868e97e21d
I.Y.G Nagendra <103441226+yogiganesh108@users.noreply.github.com>
2026-04-22 15:27:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Azure!"
+    return "this is sample python deployment in azure"
 
 if __name__ == '__main__':
     app.run()
